c85212a29ed1d3ac7a70deae6f742a009dcc88d1
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-04 15:33:51 -0700

    Initial commit with read evaluate done

---
+++ calculator.py
@@ -0,0 +1,10 @@
+"""A prefix-notation calculator.
+
+Using the arithmetic.py file from Calculator Part 1, create the
+calculator program yourself in this file.
+"""
+
+from arithmetic import *
+
+
+# Your code goes here
