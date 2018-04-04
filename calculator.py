1c33dd931b0855e1dae530967fb5b9c6b5b4e715
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-04 16:08:42 -0700

    Try using while to loop through

--- calculator.py
+++ calculator.py
@@ -5,29 +5,29 @@ calculator program yourself in this file.
 """
 
 from arithmetic import *
-
-
-# Your code goes here
 def calculator():
     while True:
         user_input = raw_input("Please enter operator command, number(s) you want to calculate.")
         token = user_input.split(" ")
-        if token[0] != "q":
-            if token[0] == "+":
-                return add(int(token[1]),int(token[2]))
-            if token[0] == "-":
-                return subtract(int(token[1]),int(token[2]))
-            if token[0] == "*":
-                return multiply(int(token[1]),int(token[2]))
-            if token[0] == "/":
-                return divide(int(token[1]),int(token[2]))
-            if token[0] == "square":
-                return square(int(token[1]))
-            if token[0] == "cube":
-                return cube(int(token[1]))
-            if token[0] == "pow":
-                return power(int(token[1]),int(token[2]))
-            if token[0] == "mod":
-                return mod(int(token[1]),int(token[2]))
-        else:
-            print("You have exited the calculator.")    
+        calculation()
+# Your code goes here
+def calculation(): 
+    if token[0] != "q":
+        if token[0] == "+":
+            return add(int(token[1]),int(token[2]))
+        if token[0] == "-":
+            return subtract(int(token[1]),int(token[2]))
+        if token[0] == "*":
+            return multiply(int(token[1]),int(token[2]))
+        if token[0] == "/":
+            return divide(int(token[1]),int(token[2]))
+        if token[0] == "square":
+            return square(int(token[1]))
+        if token[0] == "cube":
+            return cube(int(token[1]))
+        if token[0] == "pow":
+            return power(int(token[1]),int(token[2]))
+        if token[0] == "mod":
+            return mod(int(token[1]),int(token[2]))
+    else:
+        print("You have exited the calculator.")    
