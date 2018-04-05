fc75876eccd988f0279eb916907001fde62987f0
Tong Zhang <zhangtong.purdue@gmail.com>
2018-04-04 22:21:20 -0700

    Done with second method

--- calculator.py
+++ calculator.py
@@ -9,30 +9,28 @@ from arithmetic import *
 def calculator():
     while True:
         user_input = raw_input("Please enter operator command, number(s) you want to calculate.")
-        result = calculation(user_input)
-        if not result:
+        token = user_input.split(" ")
+        if token[0] == "q":
             return
         else:
-            print result 
-def calculation(user_input): 
-    user_token = user_input.split(" ")
-    token = user_token
+            print calculation(token)
+def calculation(token):   
     if token[0] != "q":
         if token[0] == "+":
-            return add(int(token[1]),int(token[2]))
+            return add(int(token[1]), int(token[2]))
         if token[0] == "-":
-            return subtract(int(token[1]),int(token[2]))
+            return subtract(int(token[1]), int(token[2]))
         if token[0] == "*":
-            return multiply(int(token[1]),int(token[2]))
+            return multiply(int(token[1]), int(token[2]))
         if token[0] == "/":
-            return divide(int(token[1]),int(token[2]))
+            return divide(int(token[1]), int(token[2]))
         if token[0] == "square":
             return square(int(token[1]))
         if token[0] == "cube":
             return cube(int(token[1]))
         if token[0] == "pow":
-            return power(int(token[1]),int(token[2]))
+            return power(int(token[1]), int(token[2]))
         if token[0] == "mod":
-            return mod(int(token[1]),int(token[2]))
+            return mod(int(token[1]), int(token[2]))
     else:
         print("You have exited the calculator.")    
